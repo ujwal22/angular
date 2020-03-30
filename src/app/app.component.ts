c0f078c060690b4c778b3f9a9dbc5bc3e8666080
ujwal22 <62586927+ujwal22@users.noreply.github.com>
2020-03-30 09:08:22 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component} from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -20,5 +23,32 @@ export class AppComponent {
   mouseMove() {
     this.xboolean = false;
     
+  }
+}
+export class FormDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  newUserForm = new FormGroup({
+
+    FirstName: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+    LastName: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)])
+  })
+  isLoginDisplay=true;
+  isLoginDisplay1=true;
+  isLoginDisplay2=true;
+  displayLogin(){
+    this.isLoginDisplay = false;
+  }
+  displayLogin1(){
+    this.isLoginDisplay = false;
+    this.isLoginDisplay1=false;
+  }
+  displayLogin2(){
+    this.isLoginDisplay=false;
+    this.isLoginDisplay2 = false;
   }
 }
